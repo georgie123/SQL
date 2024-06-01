ea9d3062cde534913f7e16d4b2d2f60a7c6308a8
georgie123 <georges.hinot@gmail.com>
2024-06-01 18:37:21 +0200

    Standard update

--- _TestData/CreateTables_2.sql
+++ _TestData/CreateTables_2.sql
@@ -1,13 +1,12 @@
 
-
 -- Postgres
 USE systeme_b ;
 
 DROP TABLE IF EXISTS prospects ;
+
 CREATE TABLE prospects
 (
-id serial NOT NULL,
-
+    id serial NOT NULL,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     email1 VARCHAR(150) NOT NULL,
